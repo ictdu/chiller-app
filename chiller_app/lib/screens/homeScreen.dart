e36c9ff727516ade8c60846ef463b816d57c7e4f
Dio <dio.allenjairone@gmail.com>
2021-02-09 15:30:37 +0800

    Added an Image Carousel Slider

--- chiller_app/lib/screens/homeScreen.dart
+++ chiller_app/lib/screens/homeScreen.dart
@@ -1,11 +1,10 @@
 import 'package:chiller_app/providers/auth_provider.dart';
-import 'package:chiller_app/providers/location_provider.dart';
-import 'package:chiller_app/screens/map_screen.dart';
 import 'package:chiller_app/screens/welcome_screen.dart';
+import 'package:chiller_app/widgets/image_slider.dart';
+import 'package:chiller_app/widgets/my_appbar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String id = 'home-screen';
@@ -16,91 +15,20 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
-  String _location = '';
-
-  @override
-  void initState() {
-    getPrefs();
-    super.initState();
-  }
-
-  getPrefs()async{
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String location = prefs.getString('location');
-    setState(() {
-      _location = location;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     final auth = Provider.of<AuthProvider>(context);
-    final locationData = Provider.of<LocationProvider>(context);
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        leading: Container(),
-        title: FlatButton(
-          onPressed: (){
-            locationData.getCurrentPosition();
-            if(locationData.permissionAllowed==true){
-              Navigator.pushNamed(context, MapScreen.id);
-            }else{
-              print('Permission is not allowed');
-            }
-          },
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Flexible(
-                child: Text(_location == null ? 'Address is not set' : _location,
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-              Icon(
-                Icons.edit_outlined,
-                color: Colors.white,),
-            ],
-          ),
-        ),
-        actions: [
-          IconButton(
-              icon: Icon(Icons.account_circle_outlined,
-              color: Colors.white,),
-              onPressed: (){
-
-              },
-          ),
-        ],
-        centerTitle: true,
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(56),
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search',
-                prefixIcon: Icon(Icons.search, color: Colors.grey,),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5),
-                  borderSide: BorderSide.none
-                ),
-                contentPadding: EdgeInsets.zero,
-                filled: true,
-                fillColor: Colors.white,
-
-              ),
-            ),
-          ),
-        ),
+      resizeToAvoidBottomInset: false,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(112),
+        child: MyAppBar(),
       ),
       body: Center(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: [
+            ImageSlider(),
             RaisedButton(
               onPressed: (){
                 auth.error='';
